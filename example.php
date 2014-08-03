264cf9c60879629dca7f817d5be56c5c0baf07ac
LuaxY <yann.guineau@voidmx.net>
2014-08-04 00:09:01 +0200

    doc(*): update example

--- example.php
+++ example.php
@@ -8,6 +8,31 @@ $data = fread($fp, filesize($file));
 
 $effects = new binaryReader($data);
 
-var_dump($effects->readByte());
-var_dump($effects->readShort());
-var_dump($effects->readShort());
+for($i = 0; $i < 3; $i++)
+{
+    $effectType = $effects->readByte();
+
+    if ($effectType == 6)
+    {
+        if ($effects->peekByte() == 67)
+        {
+            $effects->readByte();
+            echo "id: ".$effects->readShort()."\n";
+        }
+        else
+        {
+            echo "targets: ".$effects->readInt()."\n";
+            echo "id: ".$effects->readShort()."\n";
+            echo "duration: ".$effects->readInt()."\n";
+            echo "delay: ".$effects->readInt()."\n";
+            echo "random: ".$effects->readInt()."\n";
+            echo "group: ".$effects->readInt()."\n";
+            echo "modificator: ".$effects->readInt()."\n";
+            echo "trigger: ".$effects->readBool()."\n";
+            echo "hidden: ".$effects->readBool()."\n";
+            echo "raw zone: "./*$effects->readUTF().*/"\n";
+        }
+
+        echo "value: ".$effects->readShort()."\n\n";
+    }
+}
